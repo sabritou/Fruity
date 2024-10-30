d888777dfdc9fea8c0ff01ea2af9677f069036e1
sabritou <k.sabritou@gmail.com>
2024-10-30 11:51:46 +0100

    V1.001

--- slide.js
+++ slide.js
@@ -10,37 +10,34 @@ function changeSlide() {
     slide.classList.add("slide-out-right");
 
     setTimeout(() => {
-        // Retirer les classes de couleur de fond existantes
         body.classList.remove("Cherry", "Exotic", "Apple");
 
-        // Changer la couleur de fond, le texte, et les images en fonction de la slide actuelle
         if (currentSlide === 1) {
             body.classList.add("Exotic"); 
             title.textContent = "Exotic";
-            slideImage.src = "img/1.png"; // Image pour Slide 2
+            slideImage.src = "img/1.png"; 
             slideImage.alt = "Exotic";
-            slideImage2.src = "img/blackberri.png"; // Image de superposition pour Slide 2
+            slideImage2.src = "img/blackberri.png"; 
             currentSlide = 2;
         } else if (currentSlide === 2) {
             body.classList.add("Apple");
             title.textContent = "Apple";
-            slideImage.src = "img/3.png"; // Image pour Slide 3
+            slideImage.src = "img/3.png"; 
             slideImage.alt = "Apple";
-            slideImage2.src = "img/apple.png"; // Image de superposition pour Slide 3
+            slideImage2.src = "img/apple.png"; 
             currentSlide = 3;
         } else {
             body.classList.add("Cherry");
             title.textContent = "Cherry";
-            slideImage.src = "img/2.png"; // Revenir à l'image de la Slide 1
+            slideImage.src = "img/2.png"; 
             slideImage.alt = "Cherry";
-            slideImage2.src = "img/fraise.png"; // Image de superposition pour Slide 1
+            slideImage2.src = "img/fraise.png";
             currentSlide = 1;
         }
 
         slide.classList.remove("slide-out-right");
         slide.classList.add("slide-in-right");
 
-        // Enlever la classe slide-in-right après la transition pour permettre le prochain cycle
         setTimeout(() => {
             slide.classList.remove("slide-in-right");
         }, 800);
